3fbcaeefe7db262029af26f589f3416589576890
Paweł Chwała <pawel.chwala@gmail.com>
2019-12-05 14:18:24 +0100

    Make solution of day 5 more verbose & less readable ;)

--- src/main/kotlin/ml/szloch/aoc/e2019/day05/Day05.kt
+++ src/main/kotlin/ml/szloch/aoc/e2019/day05/Day05.kt
@@ -2,6 +2,113 @@ package ml.szloch.aoc.e2019.day05
 
 import ml.szloch.aoc.AoC
 
+data class ExecutionContext(
+    val ip: Int,
+    val mem: List<Int>,
+    val input: List<Int>,
+    val output: List<Int>,
+    val halted: Boolean
+)
+
+interface Operation {
+    fun execute(executionContext: ExecutionContext): ExecutionContext
+    fun opValue(mem: List<Int>, address: Int, mode: Boolean): Int {
+        return if (mode) address else mem[address]
+    }
+
+    fun lop(executionContext: ExecutionContext, mode: Boolean): Int {
+        return opValue(executionContext.mem, executionContext.mem[executionContext.ip + 1], mode)
+    }
+
+    fun rop(executionContext: ExecutionContext, mode: Boolean): Int {
+        return opValue(executionContext.mem, executionContext.mem[executionContext.ip + 2], mode)
+    }
+
+}
+
+class AddOp(private val modes: Pair<Boolean, Boolean>) : Operation {
+    override fun execute(executionContext: ExecutionContext): ExecutionContext {
+        val target = executionContext.mem[executionContext.ip + 3]
+        val newMem = executionContext.mem.toMutableList()
+        newMem[target] = lop(executionContext, modes.first) + rop(executionContext, modes.second)
+        return executionContext.copy(ip = executionContext.ip + 4, mem = newMem)
+    }
+}
+
+class MulOp(private val modes: Pair<Boolean, Boolean>) : Operation {
+    override fun execute(executionContext: ExecutionContext): ExecutionContext {
+        val target = executionContext.mem[executionContext.ip + 3]
+        val newMem = executionContext.mem.toMutableList()
+        newMem[target] = lop(executionContext, modes.first) * rop(executionContext, modes.second)
+        return executionContext.copy(ip = executionContext.ip + 4, mem = newMem)
+    }
+}
+
+
+class ReadOp : Operation {
+    override fun execute(executionContext: ExecutionContext): ExecutionContext {
+        val target = executionContext.mem[executionContext.ip + 1]
+        val newMem = executionContext.mem.toMutableList()
+        newMem[target] = executionContext.input.first()
+        return executionContext.copy(
+            ip = executionContext.ip + 2, mem = newMem, input = executionContext.input.drop(1)
+        )
+    }
+}
+
+class WriteOp(private val mode: Boolean) : Operation {
+    override fun execute(executionContext: ExecutionContext): ExecutionContext {
+        return executionContext.copy(
+            ip = executionContext.ip + 2, output = executionContext.output.plus(lop(executionContext, mode))
+        )
+    }
+}
+
+
+class JumpIfTrueOp(private val modes: Pair<Boolean, Boolean>) : Operation {
+    override fun execute(executionContext: ExecutionContext): ExecutionContext {
+        val newIp = if (lop(executionContext, modes.first) != 0) rop(
+            executionContext,
+            modes.second
+        ) else executionContext.ip + 3
+        return executionContext.copy(ip = newIp)
+    }
+}
+
+class JumpIfFalseOp(private val modes: Pair<Boolean, Boolean>) : Operation {
+    override fun execute(executionContext: ExecutionContext): ExecutionContext {
+        val newIp = if (lop(executionContext, modes.first) == 0) rop(
+            executionContext,
+            modes.second
+        ) else executionContext.ip + 3
+        return executionContext.copy(ip = newIp)
+    }
+}
+
+class LessThanOp(private val modes: Pair<Boolean, Boolean>) : Operation {
+    override fun execute(executionContext: ExecutionContext): ExecutionContext {
+        val target = executionContext.mem[executionContext.ip + 3]
+        val newMem = executionContext.mem.toMutableList()
+        newMem[target] = if (lop(executionContext, modes.first) < rop(executionContext, modes.second)) 1 else 0
+        return executionContext.copy(ip = executionContext.ip + 4, mem = newMem)
+    }
+}
+
+class EqualsOp(private val modes: Pair<Boolean, Boolean>) : Operation {
+    override fun execute(executionContext: ExecutionContext): ExecutionContext {
+        val target = executionContext.mem[executionContext.ip + 3]
+        val newMem = executionContext.mem.toMutableList()
+        newMem[target] = if (lop(executionContext, modes.first) == rop(executionContext, modes.second)) 1 else 0
+        return executionContext.copy(ip = executionContext.ip + 4, mem = newMem)
+    }
+}
+
+class HaltOp : Operation {
+    override fun execute(executionContext: ExecutionContext): ExecutionContext {
+        return executionContext.copy(halted = true)
+    }
+}
+
 class Day05 : AoC<Int, Int> {
 
     override fun firstStar(): Int {
@@ -23,86 +130,35 @@ class Day05 : AoC<Int, Int> {
             .map(String::toInt)
             .toMutableList()
 
-        val input = listOf(5)
-
-        return runProgram(memory, input).last()
+        return runProgram(memory, listOf(5)).last()
     }
 
     private fun runProgram(mem: MutableList<Int>, input: List<Int>): List<Int> {
-        var ip = 0
-        var i = 0
+        var executionContext = ExecutionContext(0, mem, input, listOf(), false)
 
-        val output = mutableListOf<Int>()
-
-        while (true) {
-            val opcodeWithModes = mem[ip]
-            val opcode = opcodeWithModes % 100
-            var modes = opcodeWithModes / 100
-            val fom = modes % 10; modes /= 10;
-            val som = modes % 10
-
-            when (opcode) {
-                99 -> {
-                    return output
-                }
-                1 -> {
-                    mem[mem[ip + 3]] = opv(mem, fom, mem[ip + 1]) + opv(mem, som, mem[ip + 2])
-                    ip += 4
-                }
-                2 -> {
-                    mem[mem[ip + 3]] = opv(mem, fom, mem[ip + 1]) * opv(mem, som, mem[ip + 2])
-                    ip += 4
-                }
-                3 -> {
-                    mem[mem[ip + 1]] = input[i]
-                    i += 1
-                    ip += 2
-                }
-                4 -> {
-                    output.add(opv(mem, fom, mem[ip + 1]))
-                    ip += 2
-                }
-                5 -> {
-                    if (opv(mem, fom, mem[ip + 1]) != 0) {
-                        ip = opv(mem, som, mem[ip + 2])
-                    } else {
-                        ip += 3
-                    }
-                }
-                6 -> {
-                    if (opv(mem, fom, mem[ip + 1]) == 0) {
-                        ip = opv(mem, som, mem[ip + 2])
-                    } else {
-                        ip += 3
-                    }
-                }
-                7 -> {
-                    if (opv(mem, fom, mem[ip + 1]) < opv(mem, som, mem[ip + 2])) {
-                        mem[mem[ip + 3]] = 1
-                    } else {
-                        mem[mem[ip + 3]] = 0
-                    }
-                    ip += 4
-                }
-                8 -> {
-                    if (opv(mem, fom, mem[ip + 1]) == opv(mem, som, mem[ip + 2])) {
-                        mem[mem[ip + 3]] = 1
-                    } else {
-                        mem[mem[ip + 3]] = 0
-                    }
-                    ip += 4
-
-                }
-                else -> throw IllegalStateException()
-            }
+        while (!executionContext.halted) {
+            executionContext = operationAt(executionContext.ip, executionContext.mem).execute(executionContext)
         }
-
+        return executionContext.output
     }
 
-    private fun opv(mem: MutableList<Int>, mode: Int, param: Int): Int {
-        return when (mode) {
-            0 -> mem[param]
-            else -> param
+    private fun operationAt(ip: Int, mem: List<Int>): Operation {
+        val opcodeAndModes = mem[ip]
+        val opcode = opcodeAndModes % 100
+        val modesEncoded = opcodeAndModes / 100
+        val modes = Pair(modesEncoded % 10 == 1, (modesEncoded / 10) % 10 == 1)
+
+        return when (opcode) {
+            1 -> AddOp(modes)
+            2 -> MulOp(modes)
+            3 -> ReadOp()
+            4 -> WriteOp(modes.first)
+            5 -> JumpIfTrueOp(modes)
+            6 -> JumpIfFalseOp(modes)
+            7 -> LessThanOp(modes)
+            8 -> EqualsOp(modes)
+            99 -> HaltOp()
+            else -> throw IllegalStateException()
         }
     }
 
