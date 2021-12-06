cdd6336d2e4fc3ed86da0888090af69d26eae20e
Paweł Chwała <pawel.chwala@gmail.com>
2021-12-06 09:31:45 +0100

    Get rid of fold

--- src/main/kotlin/pl/kaq/aoc/e2021/day06/Day06.kt
+++ src/main/kotlin/pl/kaq/aoc/e2021/day06/Day06.kt
@@ -1,31 +1,34 @@
 package pl.kaq.aoc.e2021.day06
 
 import pl.kaq.aoc.AoC
+import kotlin.reflect.KFunction1
 
 class Day06 : AoC<Long, Long> {
 
     override fun firstStar(): Long {
-        var pop = mutablePopulation()
-        repeat(80) { pop = nextPop(pop) }
-        return pop.values.sum()
+        return iterate(80, ::nextPop, readPopulation()).values.sum()
     }
 
     override fun secondStar(): Long {
-        var pop = mutablePopulation()
-        repeat(256) { pop = nextPop(pop) }
-        return pop.values.sum()
+        return iterate(256, ::nextPop, readPopulation()).values.sum()
+    }
+
+    private fun <T> iterate(times: Int, f: KFunction1<T, T>, init: T): T {
+        tailrec fun iterateRec(curr: T, counter: Int): T {
+            return if (counter == 0) curr else iterateRec(f(curr), counter - 1)
+        }
+        return iterateRec(init, times)
     }
 
     private fun nextPop(pop: Map<Int, Long>): Map<Int, Long> {
         return (0..8).associateWith { pop.getValue((it + 1) % 9) + (if (it == 6) pop.getValue(0) else 0) }
     }
 
-    private fun mutablePopulation(): Map<Int, Long> {
+    private fun readPopulation(): Map<Int, Long> {
         return inputTrimmed().split(",")
             .map { it.toInt() }
             .groupingBy { it }.eachCount()
             .mapValues { it.value.toLong() }
-            .toMutableMap()
             .withDefault { 0 }
     }
 }
