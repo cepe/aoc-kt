b55e0816a167eddda4232e4a00f0a29875f9f815
Paweł Chwała <pawel.chwala@gmail.com>
2021-12-06 09:21:31 +0100

    Make it more imperative and readable

--- src/main/kotlin/pl/kaq/aoc/e2021/day06/Day06.kt
+++ src/main/kotlin/pl/kaq/aoc/e2021/day06/Day06.kt
@@ -5,22 +5,27 @@ import pl.kaq.aoc.AoC
 class Day06 : AoC<Long, Long> {
 
     override fun firstStar(): Long {
-        return (1..80).fold(parsePopulation()) { prevPop, _ -> nextPop(prevPop) }.values.sum()
+        var pop = mutablePopulation()
+        repeat(80) { pop = nextPop(pop) }
+        return pop.values.sum()
     }
 
     override fun secondStar(): Long {
-        return (1..256).fold(parsePopulation()) { prevPop, _ -> nextPop(prevPop) }.values.sum()
+        var pop = mutablePopulation()
+        repeat(256) { pop = nextPop(pop) }
+        return pop.values.sum()
     }
 
     private fun nextPop(pop: Map<Int, Long>): Map<Int, Long> {
         return (0..8).associateWith { pop.getValue((it + 1) % 9) + (if (it == 6) pop.getValue(0) else 0) }
     }
 
-    private fun parsePopulation(): Map<Int, Long> {
+    private fun mutablePopulation(): Map<Int, Long> {
         return inputTrimmed().split(",")
             .map { it.toInt() }
             .groupingBy { it }.eachCount()
             .mapValues { it.value.toLong() }
+            .toMutableMap()
             .withDefault { 0 }
     }
 }
