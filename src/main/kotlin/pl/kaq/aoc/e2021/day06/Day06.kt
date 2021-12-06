8cd21a3940fa0fc7cb92a4960e41ffbf69a49ae5
Paweł Chwała <pawel.chwala@gmail.com>
2021-12-06 09:11:23 +0100

    Move private down

--- src/main/kotlin/pl/kaq/aoc/e2021/day06/Day06.kt
+++ src/main/kotlin/pl/kaq/aoc/e2021/day06/Day06.kt
@@ -8,12 +8,12 @@ class Day06 : AoC<Long, Long> {
         return (1..80).fold(parsePopulation()) { prevPop, _ -> nextPop(prevPop) }.values.sum()
     }
 
-    private fun nextPop(pop: Map<Int, Long>): Map<Int, Long> {
-        return (0..8).associateWith { pop.getValue((it + 1) % 9) + (if (it == 6) pop.getValue(0) else 0) }
-    }
-
     override fun secondStar(): Long {
         return (1..256).fold(parsePopulation()) { prevPop, _ -> nextPop(prevPop) }.values.sum()
+    }
+
+    private fun nextPop(pop: Map<Int, Long>): Map<Int, Long> {
+        return (0..8).associateWith { pop.getValue((it + 1) % 9) + (if (it == 6) pop.getValue(0) else 0) }
     }
 
     private fun parsePopulation(): Map<Int, Long> {
