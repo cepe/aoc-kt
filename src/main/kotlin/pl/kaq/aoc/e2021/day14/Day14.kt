b399507824a4cd47c4da176b4826a9bcd285875a
Pawel Chwala <pawel.chwala@gmail.com>
2021-12-14 11:17:43 +0100

    Solve Day 14 of AoC 2021

---
+++ src/main/kotlin/pl/kaq/aoc/e2021/day14/Day14.kt
@@ -0,0 +1,57 @@
+package pl.kaq.aoc.e2021.day14
+
+import pl.kaq.aoc.AoC
+
+class Day14 : AoC<Long, Long> {
+
+    override fun firstStar(): Long {
+        val (polymer, rules) = parseInput()
+        val (min, max) = simulate(polymer, rules, 10)
+        return max / 2 + 1 - min / 2
+    }
+
+    override fun secondStar(): Long {
+        val (polymer, rules) = parseInput()
+        val (min, max) = simulate(polymer, rules, 40)
+        return max / 2 + 1 - min / 2
+    }
+
+    private fun simulate(polymer: String, rules: Map<String, String>, stepCount: Int): Pair<Long, Long> {
+        val steps = generateSequence(polymer.windowed(2).associateWith { 1L }) { counters -> step(counters, rules) }
+        val afterSteps = steps
+            .drop(stepCount)
+            .first()
+            .flatMap { (k, v) -> listOf(k[0] to v, k[1] to v) }
+            .sumGroups()
+
+        val min = afterSteps.minOf { it.value }
+        val max = afterSteps.maxOf { it.value }
+        return Pair(min, max)
+    }
+
+    private fun <T> List<Pair<T, Long>>.sumGroups(): Map<T, Long> {
+        return this.groupBy { it.first }
+            .map { (k, v) -> k to v.sumOf { it.second } }
+            .toMap()
+    }
+
+    private fun step(counters: Map<String, Long>, rules: Map<String, String>) = counters
+        .flatMap { (k, v) ->
+            if (k in rules) listOf(k[0] + rules.getValue(k) to v, rules.getValue(k) + k[1] to v) else listOf(k to v)
+        }
+        .sumGroups()
+
+    private fun parseInput(): Pair<String, Map<String, String>> {
+        val lines = inputLines()
+        val polymer = lines[0]
+        val rules = lines
+            .drop(2)
+            .map { it.split(" -> ") }
+            .associate { it[0] to it[1] }
+        return Pair(polymer, rules)
+    }
+}
+
+fun main() {
+    Day14().solve()
+}
